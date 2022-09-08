74ef44860e3c13064edc33feafa8dc2a174a331b
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-09-07 22:16:18 -0600

    3, 4, 5

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext();
 
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
@@ -11,15 +12,36 @@ import * as React from 'react'
 //   🐨 return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
 
+function CountProvider({ children, ...otherProps }) {
+  const [ count, setCount ] = React.useState(0);
+
+  return (
+    <CountContext.Provider value={[count, setCount]} {...otherProps} >
+      {children}
+    </CountContext.Provider>
+  )
+}
+
+function useCount() {
+  const context = React.useContext(CountContext);
+  if (!context) {
+    throw new Error('CountContext must be used within CountProvider');
+  }
+
+  return context;
+}
+
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  // const [count] = React.useContext(CountContext);
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  // const [_, setCount] = React.useContext(CountContext);
+  const [ _, setCount ] = useCount();
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -31,8 +53,10 @@ function App() {
         🐨 wrap these two components in the CountProvider so they can access
         the CountContext value
       */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,22 +4,23 @@
 import * as React from 'react'
 
 // 🐨 wrap this in a React.forwardRef and accept `ref` as the second argument
-function MessagesDisplay({messages}) {
+const MessagesDisplay = React.forwardRef(({messages}, ref) => {
   const containerRef = React.useRef()
   React.useLayoutEffect(() => {
     scrollToBottom()
-  })
+  });
 
-  // 💰 you're gonna want this as part of your imperative methods
-  // function scrollToTop() {
-  //   containerRef.current.scrollTop = 0
-  // }
   function scrollToBottom() {
-    containerRef.current.scrollTop = containerRef.current.scrollHeight
+    containerRef.current.scrollTop = containerRef.current.scrollHeight;
   }
 
-  // 🐨 call useImperativeHandle here with your ref and a callback function
-  // that returns an object with scrollToTop and scrollToBottom
+  // This return object defines methods on refs, which is called callback Refs
+  React.useImperativeHandle(ref, () => {
+    return {
+      scrollToTop: () => containerRef.current.scrollTop = 0,
+      scrollToBottom
+    };
+  })
 
   return (
     <div ref={containerRef} role="log">
@@ -31,7 +32,31 @@ function MessagesDisplay({messages}) {
       ))}
     </div>
   )
-}
+})
+
+// function MessagesDisplay({messages}) {
+  // 💰 you're gonna want this as part of your imperative methods
+  // function scrollToTop() {
+  //   containerRef.current.scrollTop = 0
+  // }
+  // function scrollToBottom() {
+  //   containerRef.current.scrollTop = containerRef.current.scrollHeight
+  // }
+
+  // 🐨 call useImperativeHandle here with your ref and a callback function
+  // that returns an object with scrollToTop and scrollToBottom
+
+//   return (
+//     <div ref={containerRef} role="log">
+//       {messages.map((message, index, array) => (
+//         <div key={message.id}>
+//           <strong>{message.author}</strong>: <span>{message.content}</span>
+//           {array.length - 1 === index ? null : <hr />}
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
 
 function App() {
   const messageDisplayRef = React.useRef()
